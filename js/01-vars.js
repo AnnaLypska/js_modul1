dcce4e751b73db08dd1abd30050703d9b5cc3816
AnnaLypska <anna.lypska@gmail.com>
2020-09-23 20:36:49 +0200

    module1-task01

---
+++ js/01-vars.js
@@ -0,0 +1,28 @@
+// 'use strict';
+const name = 'Генератор защитного поля';
+// console.log(name);
+let price = 1000;
+// console.log(price);
+price = 2000;
+const a = "Выбран  " + name + ", цена за штуку " + price + " кредитов.";
+console.log (a);
+
+
+
+
+const name = '«Генератор защитного поля»'; // Write code on this line
+let price = 1000; // Write code on this line
+const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
+price = 2000;
+const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
+
+// console.log(firstMessage);
+// 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
+
+// console.log(secondMessage);
+// 'Выбран «Генератор защитного поля», цена за штуку 2000 кредитов'
+
+
+
+
+
